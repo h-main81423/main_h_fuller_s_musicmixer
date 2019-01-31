ca12af6ccb3aa3655f46d101f96977a8ff79115d
Hayden Main <hayden.main2@gmail.com>
2019-01-30 19:27:27 -0500

    initial file structure and git keep files added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//(() => {
+ 	//self-invoking anonymous function (or Lamda)
+ 	
+ 	//var => means create a variable
+ 	//whatever comes after the equal sign is what the variable
+ 	//
+ 	//document is always the webpage
+ 	//after the dot is always a method or a property
+//})();
+
+//var firstName = "Hayden";
+
+//alert("Hello " + firstName);
+
+//modern js
+
+//alert(`hello ${firstName} !!!`)
+
+console.log("JavaScript is properly linked up");
