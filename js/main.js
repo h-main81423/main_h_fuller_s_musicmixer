7131974aff6b5a55dfcbdd20e9824e6a4793132a
Simon <Simon@bbys-MacBook.local>
2019-04-06 23:32:02 -0400

    added svg and updated index and js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,60 @@
-//(() => {
- 	//self-invoking anonymous function (or Lamda)
- 	
- 	//var => means create a variable
- 	//whatever comes after the equal sign is what the variable
- 	//
- 	//document is always the webpage
- 	//after the dot is always a method or a property
-//})();
+console.log("linked JS");
 
-//var firstName = "Hayden";
+// using the Self Executing Function to limit the scope of the following variables to local
+	(function() {
+//console.log("self executing function is working");
+  
+//variablesarray
+//creating dropzone and icon,zone with local let statements
+  let dropZone = document.querySelector("#figureContainer");
+      dragIcons = document.querySelectorAll(".musicNav div");
+  
+// var animations ["figure.gif"]
+// functions list
+    function dragStart (e) {
+// using this function to transfer text data in order to start the drag process
+      e.dataTransfer.setData('text', e.target.dataset.audiosrc);
+// debugger;
+//console.log("its draggin");
+                                    
+   }
+// all three events need default to stop default from taking place before being handled
+     function dragOv(e) {
+//console.log("draggin over it")
+    
+      e.preventDefault();
+  }
 
-//alert("Hello " + firstName);
+    function dragNd() {
+      e.preventDefault();
+//console.log("stopped draggin");
+  }
+    
+  function dropped(e) {
+//console.log("dropped");
+//need default to stop default from taking place before being handled by drag over
+    e.preventDefault();
+    
+// audio element playback and creation
+    
+    let targetSrc = e.dataTransfer.getData("text");
+   
 
-//modern js
+    let audioElement = document.createElement("audio");
 
-//alert(`hello ${firstName} !!!`)
+    audioElement.src = `audio/${targetSrc}`;
+    audioElement.load();
+    audioElement.play();
 
-console.log("JavaScript is properly linked up");
+  }
+
+// event listeners
+    dragIcons.forEach(icon => {
+      icon.addEventListener("dragstart", dragStart);
+  })
+
+    dropZone.addEventListener("dragover", dragOv);    
+    dropZone.addEventListener("drop", dropped);
+
+    
+})();
